debb9dbc1afbef692c6e9ea7709deea8c4b644ad
aadityabhusal <bhusal.001aditya@gmil.com>
2021-03-19 10:48:58 +0545

    Label class refactored, label information extraction completed

--- src/scripts/Label.js
+++ src/scripts/Label.js
@@ -1,14 +1,24 @@
 export class Label {
-  constructor() {
+  constructor(deleteLabels) {
     this.video = document.getElementById("video-container");
     this.overlay = document.getElementById("video-overlay");
+    this.labelList = document.getElementById("label-list");
     this.label = null;
+    this.labelInput = null;
+    this.labelDuration = null;
+    this.removeLabel = null;
+
     this.labelX = 0;
     this.labelY = 0;
     this.id = this.overlay.children.length;
-    this.data = { title: "", description: "" };
+    this.data = "";
+    this.time = { start: 0, duration: 0 };
+    this.position = { x: 0, y: 0 };
+    this.dimension = { w: 100, h: 100 };
+
     this.color = this.generateRandomColor();
-    this.create(this.color);
+    this.createLabel(this.color);
+    this.deleteLabels = deleteLabels;
   }
 
   get labelElement() {
@@ -20,15 +30,22 @@ export class Label {
       id: this.id,
       data: this.data,
       color: this.color,
-      x: this.labelX,
-      y: this.labelY,
-      w: this.label.clientWidth,
-      h: this.label.clientHeight,
+      time: this.time,
+      position: this.position,
+      dimension: this.dimension,
     };
   }
 
   set labelInfo(data) {
     this.data = data;
+  }
+
+  get labelTime() {
+    return this.time;
+  }
+
+  set labelTime(time) {
+    this.time = time;
   }
 
   listeners = () => {
@@ -46,9 +63,15 @@ export class Label {
       this.labelY = e.pageY;
       this.checkBoundaries();
     });
+
+    this.label.addEventListener("mouseout", () => {
+      let w = this.label.clientWidth;
+      let h = this.label.clientHeight;
+      this.dimension = { w, h };
+    });
   };
 
-  create = (color) => {
+  createLabel = (color) => {
     this.label = document.createElement("div");
     this.label.id = "label" + this.id;
     this.label.classList.add("label");
@@ -56,25 +79,32 @@ export class Label {
     this.label.style.background = `rgb(${color.r}, ${color.g},${color.b})`;
     this.overlay.appendChild(this.label);
     this.listeners();
+    this.createLabelInput();
   };
 
   checkBoundaries = () => {
-    let x = this.label.offsetLeft + this.label.clientWidth;
-    let y = this.label.offsetTop + this.label.clientHeight;
+    let x = this.label.offsetLeft;
+    let y = this.label.offsetTop;
+    let w = this.label.clientWidth;
+    let h = this.label.clientHeight;
     let endX = this.video.clientWidth;
     let endY = this.video.clientHeight;
 
-    if (x > endX) {
-      this.label.style.left = endX - this.label.clientWidth + "px";
-    } else if (this.label.offsetLeft < 0) {
+    if (x + w > endX) {
+      this.label.style.left = endX - w + "px";
+    } else if (x < 0) {
       this.label.style.left = "0px";
+      x = 0;
     }
 
-    if (y > endY) {
-      this.label.style.top = endY - this.label.clientHeight + "px";
-    } else if (this.label.offsetTop < 0) {
+    if (y + h > endY) {
+      this.label.style.top = endY - h + "px";
+    } else if (y < 0) {
       this.label.style.top = "0px";
+      y = 0;
     }
+
+    this.position = { x, y };
   };
 
   generateRandomColor = () => {
@@ -84,4 +114,57 @@ export class Label {
 
     return { r, g, b };
   };
+
+  createLabelInput = () => {
+    let labelBox = document.createElement("div");
+    labelBox.classList.add("label-box");
+
+    this.labelInput = document.createElement("input");
+    this.labelInput.classList.add("labelInput");
+    this.labelInput.placeholder = "Enter label text";
+    this.labelInput.style.border = `2px solid rgb(${this.color.r}, ${this.color.g},${this.color.b})`;
+
+    this.removeLabel = document.createElement("div");
+    this.removeLabel.classList.add("closeIcon");
+    this.removeLabel.innerHTML = "&#x2716;";
+
+    this.labelInputListeners();
+    labelBox.appendChild(this.labelInput);
+    labelBox.appendChild(this.removeLabel);
+    this.labelList.appendChild(labelBox);
+  };
+
+  labelInputListeners = () => {
+    this.labelInput.addEventListener("keyup", (e) => {
+      this.data = e.target.value;
+    });
+    this.labelInput.addEventListener("focus", this.selectLabel);
+    this.labelInput.addEventListener("blur", () => this.highlightLabel(false));
+    this.removeLabel.addEventListener("click", this.deleteLabel);
+  };
+
+  selectLabel = () => {
+    this.highlightLabel(true);
+  };
+
+  highlightLabel = (state) => {
+    let boxShadow = state ? "0px 0px 10px 5px #fff" : "none";
+    let border = state ? "1px solid #222" : "none";
+    let zIndex = state ? "2" : "";
+
+    this.label.style.boxShadow = boxShadow;
+    this.label.style.border = border;
+    this.label.style.zIndex = zIndex;
+  };
+
+  deleteLabel = (e) => {
+    e.stopPropagation();
+    let parent = this.labelInput.parentElement;
+    parent.parentElement.removeChild(parent);
+    this.label.parentElement.removeChild(this.label);
+    // this.labelDuration.parentElement.removeChild(this.labelDuration);
+    this.deleteLabels(this.id);
+  };
+
+  createLabelDuration = () => {};
 }
--- src/scripts/Labels.js
+++ src/scripts/Labels.js
@@ -1,63 +1,18 @@
 import { Label } from "./Label.js";
 const addLabelBtn = document.getElementById("add-label-btn");
-const labelList = document.getElementById("label-list");
+const exportDataBtn = document.getElementById("export-data-btn");
 
-let labels = [];
-let currentLabelBox = null;
+export let labels = [];
 
 addLabelBtn.addEventListener("click", (e) => {
-  let label = new Label();
-  console.log(label.labelElement.style);
-  labels.push(label.labelInfo);
-  addNewLabel(label.labelInfo);
+  let label = new Label(deleteLabels);
+  labels.push(label);
 });
 
-const addNewLabel = (label) => {
-  let labelBox = document.createElement("div");
-  labelBox.classList.add("label-box");
-
-  let labelInput = document.createElement("input");
-  labelInput.classList.add("labelInput");
-  labelInput.placeholder = "Enter label text";
-  labelInput.style.border = `2px solid rgb(${label.color.r}, ${label.color.g},${label.color.b})`;
-  labelInput.addEventListener("focus", () => selectLabel(label));
-  labelInput.addEventListener("blur", () => highlightLabel(false));
-
-  let removeLabel = document.createElement("div");
-  removeLabel.classList.add("closeIcon");
-  removeLabel.innerHTML = "&#x2716;";
-  removeLabel.addEventListener("click", (e) => deleteLabel(e, label));
-
-  labelBox.appendChild(labelInput);
-  labelBox.appendChild(removeLabel);
-  labelList.appendChild(labelBox);
+const deleteLabels = (id) => {
+  labels = labels.filter((item) => item.labelInfo.id !== id);
 };
 
-const selectLabel = (label) => {
-  if (currentLabelBox) {
-    highlightLabel(false);
-  }
-  currentLabelBox = document.getElementById("label" + label.id);
-  highlightLabel(true);
-};
-
-const highlightLabel = (state) => {
-  let boxShadow = state ? "0px 0px 10px 5px #fff" : "none";
-  let border = state ? "1px solid #222" : "none";
-  let zIndex = state ? "2" : "";
-
-  currentLabelBox.style.boxShadow = boxShadow;
-  currentLabelBox.style.border = border;
-  currentLabelBox.style.zIndex = zIndex;
-};
-
-const deleteLabel = (e, label) => {
-  e.stopPropagation();
-  let parent = e.target.parentElement;
-  parent.parentElement.removeChild(parent);
-
-  labels = labels.filter((item) => item.id !== label.id);
-
-  let labelBox = document.getElementById("label" + label.id);
-  labelBox.parentElement.removeChild(labelBox);
-};
+exportDataBtn.addEventListener("click", () => {
+  console.log(labels.map((item) => item.labelInfo));
+});
