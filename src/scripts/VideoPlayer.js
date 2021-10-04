2850014038317df16e002eac467d7f10a0b9a15a
aadityabhusal <bhusal.001aditya@gmil.com>
2021-10-04 12:33:52 +0545

    updates svg images used inside timeupdate listener

--- src/scripts/VideoPlayer.js
+++ src/scripts/VideoPlayer.js
@@ -26,7 +26,7 @@ video.addEventListener("timeupdate", (event) => {
 
   if (video.paused || video.ended) {
     play.innerHTML =
-      '<svg height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"/></svg>';
+      '<svg height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>';
     play.title = "Play";
   } else {
     play.innerHTML =
